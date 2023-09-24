f35e9c3f596c116cb05801a0ddc962019f09e788
original-subbaman <abhisheksubba.py@gmail.com>
2023-09-24 20:30:35 +0530

    credential provider

--- app/components/menu_item.tsx
+++ app/components/menu_item.tsx
@@ -1,16 +1,20 @@
-
-interface MenuItemProps{
-    img: string,
-    title: string, 
-    isActive: boolean,
+interface MenuItemProps {
+  img: string;
+  title: string;
+  isActive: boolean;
 }
 
-
-const MenuItem = ({img, title, isActive}: MenuItemProps) => {
-    return <div className="flex hover:bg-blue-400 px-8 rounded-lg py-2 items-start justify-start mb-4">
-        <img className="w-5 h-5 mr-4" src={img}/>
-        {isActive ? <p className="text-xl font-bold">{title}</p> : <p className="text-xl font-thin">{title}</p>}
+const MenuItem = ({ img, title, isActive }: MenuItemProps) => {
+  return (
+    <div className="flex hover:bg-blue-400 px-8 rounded-lg py-2 items-start justify-start mb-4">
+      <img className="w-5 h-5 mr-4" src={img} />
+      {isActive ? (
+        <p className="text-xl font-bold">{title}</p>
+      ) : (
+        <p className="text-xl font-thin">{title}</p>
+      )}
     </div>
-}
+  );
+};
 
-export default MenuItem; 
+export default MenuItem;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { getServerSession } from 'next-auth'
-import { options } from "./api/auth/[...nextauth]/options"
-import DashboardScreen from './dashboard/page'
-import LogInScreen from './login/page'
+import { getServerSession } from "next-auth";
+import { options } from "./api/auth/[...nextauth]/options";
+import DashboardScreen from "./dashboard/page";
+import LogInScreen from "./login/page";
 
 export default async function Home() {
-  const session = await getServerSession(options)
-  return (
-    session ? <DashboardScreen /> : <LogInScreen />
-  )
+  const session = await getServerSession(options);
+  return session ? <DashboardScreen /> : <LogInScreen />;
 }
